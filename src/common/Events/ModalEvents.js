174a7758f2af706ff2c4fb52c480d493c0b43476
Pamparau Costelus <pamparaucostelus1234@gmail.com>
2020-07-17 16:08:36 +0300

    Event page

--- src/common/Events/ModalEvents.js
+++ src/common/Events/ModalEvents.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
-import { Header, Image, Modal, Form, Icon, Input } from "semantic-ui-react";
+import Dropzone from "react-dropzone";
+import { FileDrop } from "react-file-drop";
+import { useDropzone } from "react-dropzone";
+import {
+  Header,
+  Image,
+  Modal,
+  Form,
+  Icon,
+  Input,
+  FormGroup,
+} from "semantic-ui-react";
 import "./ModalEvents.css";
 import Button from "../Button";
 
@@ -7,61 +18,79 @@ class ModalEvents extends Component {
   render() {
     return (
       <Modal
+        size="large"
         className="modal-events"
         open={this.props.handleOpenModal}
         close={this.props.handleCloseModal}
       >
-        <Form>
-          <h2>Add Event</h2>
-          <hr></hr>
+        <div className="modal-content-div">
+          <Form>
+            <h2>{this.props.NameModalEvents}</h2>
+            <hr></hr>
 
-          <div className="form-events">
-            <Form.Input fluid label="Name" placeholder="Input placeholder" />
+            <div className="form-events">
+              <Form.Input label="Name" placeholder="Input placeholder" />
 
-            <div className="input-time-date">
-              <Input
-                className="input-t-d"
-                icon
+              <div className="input-time-date">
+                <FormGroup inline="true">
+                  <Form.Input
+                    label="Time"
+                    icon="clock outline"
+                    placeholder="Input placeholder"
+                  />
+
+                  <Form.Input
+                    label="Date"
+                    icon="calendar alternate outline"
+                    placeholder="Search..."
+                  />
+                </FormGroup>
+              </div>
+              <Form.Select
+                className="input-description"
+                label="Location"
                 placeholder="Input placeholder"
-                label="Time"
-              >
-                <input />
-                <Icon name="calendar" />
-              </Input>
-              <Input
-                icon
-                className="input-t-d"
-                placeholder="Search..."
-                label="Description"
-              >
-                <input />
-                <Icon name="time" />
-              </Input>
-              <Form.Select label="Location" placeholder="Input placeholder" />
-            </div>
+              />
 
-            <Form.Input
-              className="description-input"
-              style={{ height: "144px" }}
-              fluid
-              label="Description"
-              placeholder="Input placeholder"
-            />
-          </div>
-          <div className="bottom-add-edit">
-            <hr />
-            <div className="button-add-events">
-              <button
-                className="button-close"
-                onClick={this.props.handleCloseModal}
-                inverted
-              >
-                Close
-              </button>
-              <Button name="Add"></Button>
+              <Form.Input
+                className="description-input"
+                style={{ height: "144px" }}
+                label="Description"
+                placeholder="Input placeholder"
+              />
+              <Dropzone onDrop={(files) => console.log(files)}>
+                {({ getRootProps, getInputProps }) => (
+                  <div className="container-event">
+                    <div
+                      {...getRootProps({
+                        className: "dropzone",
+                        onDrop: (event) => event.stopPropagation(),
+                      })}
+                    >
+                      <input {...getInputProps()} />
+                      <p>
+                        Drag 'n' drop some files here, or click to select files
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </Dropzone>
             </div>
-          </div>
-        </Form>
+            <div className="bottom-add-edit">
+              <hr />
+              <div className="button-add-events">
+                <button
+                  className="button-close"
+                  onClick={this.props.handleCloseModal}
+                  inverted
+                >
+                  Close
+                </button>
+                <Button name="Add"></Button>
+              </div>
+            </div>
+          </Form>
+        </div>
       </Modal>
     );
   }
--- src/common/Events/SelectedEvents/SelectedEvents.js
+++ src/common/Events/SelectedEvents/SelectedEvents.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ImageEvent from "./Rectangle30.png";
 import "./SelectedEvents.css";
+import ModalEvents from "../ModalEvents";
+
 import {
   Grid,
   GridColumn,
@@ -13,9 +15,41 @@ import {
 import ShowPersonEvent from "./ShowPersonEvent/ShowPersonEvent";
 
 class SelectedEvents extends Component {
+  state = { show: false };
+
+  handleOpenModal = () => {
+    this.setState({ show: true });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ show: false });
+  };
+
   render() {
     return (
       <div className="cards-event">
+        <div className="label-event">
+          <h3>{"Events > "}</h3>
+          <h4> Running for life</h4>
+        </div>
+        <div className="div-event-button-edit">
+          <button className="event-button-edit" onClick={this.handleOpenModal}>
+            EDIT
+          </button>
+        </div>
+        <div className="event-bar">
+          <Icon name="calendar alternate outline" />
+          <p>20.06.2020</p>
+
+          <div className="time-event">
+            <Icon name="clock outline" />
+            <p>09:00 AM</p>
+          </div>
+          <div className="location-event">
+            <Icon name="location arrow " />
+            <p>Suceava fortress, Main Enter</p>
+          </div>
+        </div>
         <div className="card-event">
           <Card centered fluid>
             <Image src={ImageEvent} wrapped ui={false} />
@@ -24,24 +58,32 @@ class SelectedEvents extends Component {
                 <h3>
                   this.props.FirstParagraphdbgvbsidybbfsaduougfadnilsisffadhiJSGDFHAID
                 </h3>
-
                 <p>
                   this.props.SecondParagraphsdvdbdbdvcsbfbdsybusybfubsdbcusbcusbdvgykusdvbusuidvbousivvbisudvbusdibvnsoubsdvinoduvidbohjnmklkjashugftgvbhnjmk,lsxhbdfvvghujikoijuhygtfrdesdrftgyhujikolxkoasjihucygdrtfyguh
                 </p>
               </div>
             </Card.Content>
           </Card>
-          <div className="show-person-parent">
-            <ShowPersonEvent />
-            <ShowPersonEvent />
-            <ShowPersonEvent />
-            <ShowPersonEvent />
-            <ShowPersonEvent />
-            <ShowPersonEvent />
-            <ShowPersonEvent />
-            <ShowPersonEvent />
-          </div>
         </div>
+        <div className="show-person-parent">
+          <ShowPersonEvent />
+          <ShowPersonEvent />
+          <ShowPersonEvent />
+          <ShowPersonEvent />
+          <ShowPersonEvent />
+          <ShowPersonEvent />
+          <ShowPersonEvent />
+        </div>
+
+        <div className="cards-features">
+          <h3>Participants ({this.props.CountNumberParticipantsEvent})</h3>
+          <button className="event-button-compare">Compare Performance</button>
+        </div>
+        <ModalEvents
+          NameModalEvents="Edit Event"
+          handleOpenModal={this.state.show}
+          handleCloseModal={this.handleCloseModal}
+        />
       </div>
     );
   }
